e65f84a0ef67e95910f6241d6235193c05899cf7
Sweta Singh <sweta.singh0322@gmail.com>
2026-08-03 10:44:40 +0530

    Dynamic Authorization Token generation

--- src/test/java/com/api/tests/UserDetailsAPITests.java
+++ src/test/java/com/api/tests/UserDetailsAPITests.java
@@ -1,5 +1,7 @@
 package com.api.tests;
 
+import static com.api.utils.AuthTokenProvider.getToken;
+import static com.api.utils.ConfigManager.getProperty;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.lessThan;
@@ -8,7 +10,7 @@ import java.io.IOException;
 
 import org.testng.annotations.Test;
 
-import static com.api.utils.ConfigManager.*;
+import static com.api.constant.Role.*;
 
 import io.restassured.http.ContentType;
 import io.restassured.http.Header;
@@ -17,7 +19,7 @@ import io.restassured.module.jsv.JsonSchemaValidator;
 public class UserDetailsAPITests {
 	@Test(description = "Verify user details API with valid credentials")
 	public void userDetailsAPITest() throws IOException {
-		Header authHeader = new Header("Authorization", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiZmlyc3RfbmFtZSI6ImZkIiwibGFzdF9uYW1lIjoiZmQiLCJsb2dpbl9pZCI6ImlhbWZkIiwibW9iaWxlX251bWJlciI6Ijg4OTk3NzY2NTUiLCJlbWFpbF9pZCI6Im1hcmtAZ21haWwuY29tIiwicGFzc3dvcmQiOiI1ZjRkY2MzYjVhYTc2NWQ2MWQ4MzI3ZGViODgyY2Y5OSIsInJlc2V0X3Bhc3N3b3JkX2RhdGUiOm51bGwsImxvY2tfc3RhdHVzIjowLCJpc19hY3RpdmUiOjEsIm1zdF9yb2xlX2lkIjo1LCJtc3Rfc2VydmljZV9sb2NhdGlvbl9pZCI6MSwiY3JlYXRlZF9hdCI6IjIwMjEtMTEtMDNUMDg6MDY6MjMuMDAwWiIsIm1vZGlmaWVkX2F0IjoiMjAyMS0xMS0wM1QwODowNjoyMy4wMDBaIiwicm9sZV9uYW1lIjoiRnJvbnREZXNrIiwic2VydmljZV9sb2NhdGlvbiI6IlNlcnZpY2UgQ2VudGVyIEEiLCJpYXQiOjE3ODM5MjI5MjN9.mj5Lv8UntULpmuI_6KdM8F-wHp-ubBhehkbE2SxA68U");
+		Header authHeader = new Header("Authorization", getToken(FD));
 		
         given()
         	.baseUri(getProperty("BASE_URI"))
