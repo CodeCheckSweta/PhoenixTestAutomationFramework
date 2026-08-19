6ea1e823c527983a925c8e7b60ab05209ffb0756
Sweta Singh <sweta.singh0322@gmail.com>
2026-08-19 16:09:04 +0530

    DateTimeUtil Class:
    Created DateTimeUtil Class to provide us with a past ISO standard UTC
    time

--- src/test/java/com/api/tests/CreateJobAPITest.java
+++ src/test/java/com/api/tests/CreateJobAPITest.java
@@ -3,9 +3,7 @@ package com.api.tests;
 import static com.api.constant.Role.FD;
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.hamcrest.Matchers.startsWith;
+import static org.hamcrest.Matchers.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,27 +15,28 @@ import com.api.request.model.Customer;
 import com.api.request.model.CustomerAddress;
 import com.api.request.model.CustomerProduct;
 import com.api.request.model.Problems;
-import com.api.utils.SpecUtil;
+import static com.api.utils.DateTimeUtil.*;
+import static com.api.utils.SpecUtil.*;
 
 public class CreateJobAPITest {
 	
 	@Test
 	public void createJobAPITest() {
-
+		
 		Customer customer = new Customer("Camron", "Rohan", "900-855-0363", "", "angelsweta03@gmail.com","");
 		CustomerAddress customerAddress = new CustomerAddress("c 304", "Jupiter", "MG road", "Bangur Nagar", "Goregaon West", "411039", "India", "Maharashtra");
-		CustomerProduct customerProduct = new CustomerProduct("2025-04-06T18:30:00.000Z", "45787151381711", "45787151381711", "45787151381711", "2025-04-06T18:30:00.000Z", 1, 1);
+		CustomerProduct customerProduct = new CustomerProduct(getTimeWithDaysAgo(10), "44787151381711", "44787151381711", "44787151381711", getTimeWithDaysAgo(10), 1, 1);
 		Problems problems = new Problems(1, "Battery Issue");
 		List<Problems> problemList = new ArrayList<Problems>();
 		problemList.add(problems);
 		
 		CreateJobPayload createJobPayload = new CreateJobPayload(0, 2, 1, 1, customer, customerAddress, customerProduct, problemList);
 		
-		given().spec(SpecUtil.requestSpecWithAuth(FD,createJobPayload))
+		given().spec(requestSpecWithAuth(FD,createJobPayload))
 			.when()
 			.post("/job/create")
 			.then()
-			.spec(SpecUtil.responseSpec_OK())
+			.spec(responseSpec_OK())
 			.body(matchesJsonSchemaInClasspath("response-schema/CreateJobAPIResponseSchema.json"))
 			.body("message", equalTo("Job created successfully. "))
 			.body("data.id", notNullValue())
