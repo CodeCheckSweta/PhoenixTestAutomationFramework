fb75b66a32ac7842149c17134b313ebf11d50e4f
Sweta Singh <sweta.singh0322@gmail.com>
2026-08-20 08:03:21 +0530

    Introduced ENUMs for handling the code for the Create Job API test
    Reduced magic number issue in the test scripts

--- src/test/java/com/api/tests/CreateJobAPITest.java
+++ src/test/java/com/api/tests/CreateJobAPITest.java
@@ -10,6 +10,13 @@ import java.util.List;
 
 import org.testng.annotations.Test;
 
+import com.api.constant.Model;
+import com.api.constant.OEM;
+import com.api.constant.Platform;
+import com.api.constant.Problem;
+import com.api.constant.Product;
+import com.api.constant.ServiceLocation;
+import com.api.constant.Warranty_Status;
 import com.api.request.model.CreateJobPayload;
 import com.api.request.model.Customer;
 import com.api.request.model.CustomerAddress;
@@ -19,29 +26,29 @@ import static com.api.utils.DateTimeUtil.*;
 import static com.api.utils.SpecUtil.*;
 
 public class CreateJobAPITest {
-	
+
 	@Test
 	public void createJobAPITest() {
-		
-		Customer customer = new Customer("Camron", "Rohan", "900-855-0363", "", "angelsweta03@gmail.com","");
-		CustomerAddress customerAddress = new CustomerAddress("c 304", "Jupiter", "MG road", "Bangur Nagar", "Goregaon West", "411039", "India", "Maharashtra");
-		CustomerProduct customerProduct = new CustomerProduct(getTimeWithDaysAgo(10), "44787151381711", "44787151381711", "44787151381711", getTimeWithDaysAgo(10), 1, 1);
-		Problems problems = new Problems(1, "Battery Issue");
+
+		Customer customer = new Customer("Camron", "Rohan", "900-855-0363", "", "angelsweta03@gmail.com", "");
+		CustomerAddress customerAddress = new CustomerAddress("c 304", "Jupiter", "MG road", "Bangur Nagar",
+				"Goregaon West", "411039", "India", "Maharashtra");
+		CustomerProduct customerProduct = new CustomerProduct(getTimeWithDaysAgo(10), "14787151381711",
+				"14787151381711", "14787151381711", getTimeWithDaysAgo(10), Product.NEXUS_2.getCode(),
+				Model.NEXUS_2_BLUE.getCode());
+		Problems problems = new Problems(Problem.SMARTPHONE_IS_RUNNING_SLOW.getCode(), "Battery Issue");
 		List<Problems> problemList = new ArrayList<Problems>();
 		problemList.add(problems);
-		
-		CreateJobPayload createJobPayload = new CreateJobPayload(0, 2, 1, 1, customer, customerAddress, customerProduct, problemList);
-		
-		given().spec(requestSpecWithAuth(FD,createJobPayload))
-			.when()
-			.post("/job/create")
-			.then()
-			.spec(responseSpec_OK())
-			.body(matchesJsonSchemaInClasspath("response-schema/CreateJobAPIResponseSchema.json"))
-			.body("message", equalTo("Job created successfully. "))
-			.body("data.id", notNullValue())
-			.body("data.mst_service_location_id", equalTo(1))
-			.body("data.job_number", startsWith("JOB_"));
+
+		CreateJobPayload createJobPayload = new CreateJobPayload(ServiceLocation.SERVICE_LOCATION_A.getCode(),
+				Platform.FRONT_DESK.getCode(), Warranty_Status.IN_WARRANTY.getCode(), OEM.GOOGLE.getCode(), customer,
+				customerAddress, customerProduct, problemList);
+
+		given().spec(requestSpecWithAuth(FD, createJobPayload)).when().post("/job/create").then()
+				.spec(responseSpec_OK())
+				.body(matchesJsonSchemaInClasspath("response-schema/CreateJobAPIResponseSchema.json"))
+				.body("message", equalTo("Job created successfully. ")).body("data.id", notNullValue())
+				.body("data.mst_service_location_id", equalTo(1)).body("data.job_number", startsWith("JOB_"));
 
 	}
 }
